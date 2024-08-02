540f289c95c730dd04176884ac50db07d71f6371
fang cheng <www.tkzc@foxmail.com>
2024-08-02 18:36:10 +0800

    升级到1.80.0

--- src/main.rs
+++ src/main.rs
@@ -20,34 +20,43 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Box::new(npipe_web::TemplateApp::new(cc))),
+        Box::new(|cc| Ok(Box::new(npipe_web::TemplateApp::new(cc)))),
     )
 }
 
 // When compiling to web using trunk:
 #[cfg(target_arch = "wasm32")]
-
 fn main() {
     // Redirect `log` message to `console.log` and friends:
     eframe::WebLogger::init(log::LevelFilter::Debug).ok();
 
-    if let Some(window) = web_sys::window() {
-        let document = window.document().expect("should have a document on window");
-        if let Some(element) = document.get_element_by_id("center_text") {
-            element.set_inner_html(""); // Remove the loading animation
-        }
-    }
-
     let web_options = eframe::WebOptions::default();
 
     wasm_bindgen_futures::spawn_local(async {
-        eframe::WebRunner::new()
+        let start_result = eframe::WebRunner::new()
             .start(
-                "the_canvas_id", // hardcode it
+                "the_canvas_id",
                 web_options,
-                Box::new(|cc| Box::new(npipe_web::TemplateApp::new(cc))),
+                Box::new(|cc| Ok(Box::new(npipe_web::TemplateApp::new(cc)))),
             )
-            .await
-            .expect("failed to start eframe");
+            .await;
+
+        // Remove the loading text and spinner:
+        let loading_text = web_sys::window()
+            .and_then(|w| w.document())
+            .and_then(|d| d.get_element_by_id("loading_text"));
+        if let Some(loading_text) = loading_text {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
     });
 }
